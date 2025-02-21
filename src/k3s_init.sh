0e8defa69014c02f16a058f916cadb9d602041b8
Joshua Edward McLaughlin Cox <josh@webhosting.coop>
2025-02-20 19:10:41 -0600

    k3s_init.sh

---
+++ src/k3s_init.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+SECRET=$(cat /var/lib/rancher/k3s/server/token)
+THIS_FILE=/root/k3s_init_node.sh
+
+echo '#!/bin/bash
+export THIS_IP=$(curl icanhazip.com)
+' >> ${THIS_FILE}
+
+cat <<EOF > ${THIS_FILE}
+export K3S_TOKEN=${SECRET}
+curl -sfL https://get.k3s.io | sh -s - server \
+    --cluster-init \
+EOF
+
+echo '    --tls-san=${THIS_IP}
+' >> ${THIS_FILE}
