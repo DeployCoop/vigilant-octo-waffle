b2c95c4f194e6f67cb3d196dcedceea9f1ded95b
Joshua Edward McLaughlin Cox <josh@webhosting.coop>
2025-05-27 04:17:36 -0500

    THIS_EXTERNAL_REGISTRY_SECRET

--- src/dockerCreds.sh
+++ src/dockerCreds.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 set -a && source ./.env && set +a
-kubectl create secret docker-registry harborexamplecomcred \
+
+kubectl get secrets -n ${THIS_NAMESPACE} ${THIS_EXTERNAL_REGISTRY_SECRET} &> /dev/null
+if [[ $? -eq 0 ]]; then
+  echo 'WARN: secret already exists.'
+  exit 0
+else
+  kubectl create secret docker-registry ${THIS_EXTERNAL_REGISTRY_SECRET} \
   -n ${THIS_NAMESPACE} \
   --docker-server=${THIS_EXTERNAL_REGISTRY_HOST} \
   --docker-username=${THIS_EXTERNAL_REGISTRY_USERNAME} \
   --docker-password=${THIS_EXTERNAL_REGISTRY_PASS} \
   --docker-email=${THIS_EXTERNAL_REGISTRY_EMAIL}
+fi
+exit 0
