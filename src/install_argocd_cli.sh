29b5f0b4483b77ea521dbf1bec4378b498a4a2e7
Joshua Edward McLaughlin Cox <josh@webhosting.coop>
2025-05-26 21:00:34 -0500

    src/install_argocd_cli.sh

---
+++ src/install_argocd_cli.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+ARGOCD_VERSION=$(curl --silent "https://api.github.com/repos/argoproj/argo-cd/releases/latest" | grep '"tag_name"' | sed -E 's/.*"([^"]+)".*/\1/')
+TMP=$(mktemp -d)
+trap 'rm -Rf $TMP' EXIT
+
+curl -sSL -o ${TMP}/argocd-${ARGOCD_VERSION} https://github.com/argoproj/argo-cd/releases/download/${ARGOCD_VERSION}/argocd-linux-amd64
+chmod +x ${TMP}/argocd-${VERSION}
+sudo mv ${TMP}/argocd-${VERSION} /usr/local/bin/argocd
