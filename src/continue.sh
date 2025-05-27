c6b0d5befcdc8aab3d78db9a2e2473696c93f1c0
Joshua Edward McLaughlin Cox <josh@webhosting.coop>
2025-05-27 03:48:40 -0500

    argoRunner implementation

--- src/continue.sh
+++ src/continue.sh
@@ -24,12 +24,12 @@ main () {
 
   set -u
   ${this_cwd}/src/harbor.sh
-  envsubst < argo/kube-prometheus-stack/argocd.yaml | argocd app create --name example-prometheus-stack --grpc-web -f - 
+  ${this_cwd}/src/kube-prometheus-stack.sh
   ${this_cwd}/src/openldap.sh
   ${this_cwd}/src/openproject.sh
-  envsubst < argo/nc/argocd.yaml | argocd app create --name examplenc --grpc-web -f -
-  envsubst < argo/supabase/argocd.yaml | argocd app create --name supabase --grpc-web -f -
-  envsubst < argo/bao/argocd.yaml | argocd app create --name openbao --grpc-web -f -
+  ${this_cwd}/src/nextcloud.sh
+  ${this_cwd}/src/supabase.sh
+  ${this_cwd}/src/bao.sh
   initializer "${this_cwd}/init/bao"
   ${this_cwd}/src/nextjs-docker.sh
 }
