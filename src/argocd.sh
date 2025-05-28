3851f1a9663e26b2280fefa6cf161be1cf042b9f
Joshua Edward McLaughlin Cox <josh@webhosting.coop>
2025-05-28 12:41:59 -0500

    Adding helm argo method

--- src/argocd.sh
+++ src/argocd.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
-#kubectl delete -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+TMP=$(mktemp -d)
+trap 'rm -Rf $TMP' EXIT
+set -eu
+set -a && source ./.env && set +a
+#set -x
+envsubst < argo/argo-cd/values.yaml > "${TMP}/values.yaml"
+if [[ ${THIS_ARGO_METHOD} == 'helm' ]]; then
+  helm repo add argo https://argoproj.github.io/argo-helm
+  helm repo update
+  helm install archocd argo/argo-cd -f "${TMP}/values.yaml"
+else
+  #kubectl delete -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+  kubectl create namespace argocd
+  kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+fi
 
 echo argocd admin initial-password -n argocd
