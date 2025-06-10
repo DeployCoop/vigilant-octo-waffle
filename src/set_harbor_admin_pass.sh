3b4ea98945347f475d6ffefb5de67bf48b30197e
Joshua Edward McLaughlin Cox <josh@webhosting.coop>
2025-06-09 20:06:54 -0500

    Adding mkdb.sh

--- src/set_harbor_admin_pass.sh
+++ src/set_harbor_admin_pass.sh
@@ -6,7 +6,7 @@ updaterrr () {
   HARBOR_ADMIN_PASSWORD=$(kubectl get secret -n example example-secrets -o json|jq -r '.data.HARBOR_ADMIN_PASSWORD'|base64 -d)
   UPDATE_CMD=$(./src/harbor_pass.py "${HARBOR_ADMIN_PASSWORD}")
   #echo kubectl exec -it -n ${THIS_NAMESPACE} goharbor-${THIS_NAME}-database-0 -c database -- ${UPDATE_CMD}
-  echo kubectl exec -it -n ${THIS_NAMESPACE} goharbor-${THIS_NAME}-database-0 -c database -- ${UPDATE_CMD} | bash
+  echo kubectl exec -n ${THIS_NAMESPACE} goharbor-${THIS_NAME}-database-0 -c database -- ${UPDATE_CMD} | bash
   #kubectl exec -it -n ${THIS_NAMESPACE} goharbor-${THIS_NAME}-database-0 -c database -- /bin/sh -c "${UPDATE_CMD}"
 }
 
