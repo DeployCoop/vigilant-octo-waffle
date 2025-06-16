1e451a9d6fbe7c022b71e1cb2063b65937b353c1
Joshua Edward McLaughlin Cox <josh@webhosting.coop>
2025-06-16 14:11:39 -0500

    opensearch

---
+++ src/opensearch.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+THIS_THING=opensearch
+source src/common.sh
+source src/sourceror.bash
+PASS_LENGTH=33
+if [[ -z ${THIS_OPENSEARCH_ADMIN_PASSWORD} ]]; then
+  echo 'WARN: env var THIS_OPENSEARCH_ADMIN_PASSWORD not set generating a random password!'
+  export THIS_OPENSEARCH_ADMIN_PASSWORD=$(pwgen -y -c ${PASS_LENGTH} 1)
+   pwgen -y -c 23 1
+fi
+export THIS_OPENSEARCH_ADMIN_PASSHASH=$(src/opensearch-hashpass.py ${THIS_OPENSEARCH_ADMIN_PASSWORD})
+
+secret_maker () {
+  local secret_name=$1
+  local secret_pass=$2
+  kubectl create secret generic \
+    "${secret_name}" \
+    -n "${THIS_NAMESPACE}" \
+    --from-literal=OPENSEARCH_INITIAL_ADMIN_PASSWORD="${secret_pass}"
+}
+
+main () {
+  if [[ ${VERBOSITY} -gt 99 ]]; then
+    set -x
+  fi
+  set -eu
+  secret_maker "${THIS_OPENSEARCH_ADMIN_CRED_SECRET}" "${THIS_OPENSEARCH_ADMIN_PASSWORD}"
+  argoRunner "${THIS_THING}"
+}
+time main
