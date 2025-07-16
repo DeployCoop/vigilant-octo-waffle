2aa27ccdb4a5ca53e3d0775e132022a0685faf27
Joshua Edward McLaughlin Cox <josh@webhosting.coop>
2025-07-16 16:49:53 -0500

    openproj

--- src/openproject-get-admin-pass.sh
+++ src/openproject-get-admin-pass.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 source src/sourceror.bash
+if [[ ${VERBOSITY} -gt 99 ]]; then
+  set -x
+fi
 secret_getter "${THIS_NAMESPACE}" "${THIS_OPENPROJECT_ADMIN_CRED_SECRET}" password
--- src/openproject.sh
+++ src/openproject.sh
@@ -2,7 +2,9 @@
 THIS_THING=openproject
 source src/common.sh
 
+set +e
 secret_maker "${THIS_OPENPROJECT_ADMIN_CRED_SECRET}" "${THIS_OPENPROJECT_ADMIN_USER}" "${THIS_OPENPROJECT_ADMIN_PASSWORD}"
 db_password=$(kubectl get secret -n "${THIS_NAMESPACE}" example-secrets -o json|jq -r '.data."op-db-password"'|base64 -d)
 src/mkdb.sh "${THIS_OPENPROJECT_POSTGRES_DB}" "${THIS_OPENPROJECT_POSTGRES_USER}" "${db_password}"
+set -e
 argoRunner "$THIS_THING"
