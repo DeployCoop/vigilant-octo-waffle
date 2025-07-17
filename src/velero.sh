de722b8608c3f4f2856d4ae72caf9cb322f854f1
Joshua Edward McLaughlin Cox <josh@webhosting.coop>
2025-07-16 20:25:15 -0500

    velero

---
+++ src/velero.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+THIS_THING=velero
+source src/common.sh
+
+main () {
+  set -eu
+  #initializer "${this_cwd}/init/pre-${THIS_THING}"
+  argoRunner "${THIS_THING}"
+  #w8_pod "${THIS_NAMESPACE}" "opensearch-operator-${THIS_NAME}-controller-manager"
+  #initializer "${this_cwd}/init/${THIS_THING}"
+}
+time main
