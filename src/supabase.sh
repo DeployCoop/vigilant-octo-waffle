d30a8a25d6ef934fa42a20762b6c451baad0b4e5
Joshua Edward McLaughlin Cox <josh@webhosting.coop>
2025-08-02 03:37:31 -0500

    pre

--- src/supabase.sh
+++ src/supabase.sh
@@ -5,8 +5,9 @@ source src/common.sh
 main () {
   #set -eux
   set -eu
+  initializer "${this_cwd}/init/pre-${THIS_THING}"
   argoRunner "$THIS_THING"
-  initializer "${this_cwd}/init/${THIS_THING}"
+  #initializer "${this_cwd}/init/${THIS_THING}"
 }
 
 time main
