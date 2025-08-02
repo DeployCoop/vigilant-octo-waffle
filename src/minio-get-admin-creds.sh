6df71dec211f1966e960f4de6018b2b8100c8bcc
Joshua Edward McLaughlin Cox <josh@webhosting.coop>
2025-08-02 10:02:18 -0500

    fix

--- src/minio-get-admin-creds.sh
+++ src/minio-get-admin-creds.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 source src/sourceror.bash
 
-secret_getter "${THIS_NAMESPACE}" ${THIS_NAME}-minio-env-configuration config.env
+#secret_getter "${THIS_NAMESPACE}" ${THIS_NAME}-minio-env-configuration config.env
+kubectl get secret -n bokbot bokbot-minio-env-configuration -o json|jq '.data."config.env"'|sed 's/"//g'|base64 -d
