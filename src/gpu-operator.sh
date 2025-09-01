205a382c6d8fb14b59b357b51475da129ae6ac99
Joshua Edward McLaughlin Cox <josh@webhosting.coop>
2025-09-01 09:06:15 -0500

    order

--- src/gpu-operator.sh
+++ src/gpu-operator.sh
@@ -4,8 +4,8 @@ source src/common.sh
 
 main () {
   set -eu
-  kubectl label --overwrite ns gpu-operator pod-security.kubernetes.io/enforce=privileged
   initializer "${this_cwd}/init/pre-${THIS_THING}"
+  kubectl label --overwrite ns gpu-operator pod-security.kubernetes.io/enforce=privileged
   argoRunner "${THIS_THING}"
   #w8_pod "${THIS_NAMESPACE}" "opensearch-operator-${THIS_NAME}-controller-manager"
   #initializer "${this_cwd}/init/${THIS_THING}"
