75a15c6e7b9f6128fee68a9a9f6dd5917f65b0b5
Joshua Edward McLaughlin Cox <josh@webhosting.coop>
2025-10-18 21:55:24 -0500

    adding download ignore to alpine

--- src/install_k3s.sh
+++ src/install_k3s.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+. /etc/os-release
+if [[ ID == alpine ]]; then
+  export INSTALL_K3S_SKIP_DOWNLOAD=true
+fi
+
 export THIS_IP=$(curl icanhazip.com)
 export INSTALL_K3S_EXEC="server --tls-san $THIS_IP --cluster-init --flannel-backend=wireguard-native --disable=traefik"
 curl -sfL https://get.k3s.io | sh -s -
