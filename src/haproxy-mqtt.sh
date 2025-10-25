71ebf8e419e9374fe621eeefb2f62be78a30f68e
Joshua Edward McLaughlin Cox <josh@webhosting.coop>
2025-10-25 09:49:29 -0500

    templates

--- src/haproxy-mqtt.sh
+++ src/haproxy-mqtt.sh
@@ -1,15 +1,11 @@
 #!/usr/bin/env bash
-source ./src/sourceror.bash
-TMP=$(mktemp)
-trap 'rm ${TMP}' EXIT
-envsubst < ${HAPROXY_CONFIG_TPL} > ${TMP}
 #helm upgrade --install haproxy-kubernetes-ingress haproxytech/kubernetes-ingress \
 helm upgrade --install haproxy-kubernetes-ingress kubernetes-ingress \
   --repo https://haproxytech.github.io/helm-charts \
   --wait \
   --set controller.image.tag=3.0 \
   --namespace ingress-haproxy --create-namespace \
-  -f src/ingress-haproxy-values.yaml
+  -f src/ingress-haproxy-mqtt-values.yaml
 
 source ./src/w8.bash
 #w8_pod ingress-haproxy ingress-haproxy-controller
