f30ed174bc6de8debb0686e018c66f259fcd0c51
Joshua Edward McLaughlin Cox <josh@webhosting.coop>
2025-11-04 16:10:43 -0600

    init

--- src/k3s_init.sh
+++ src/k3s_init.sh
@@ -6,5 +6,6 @@ SERVER_IP=$(curl icanhazip.com)
 echo '#!/bin/bash' > ${THIS_FILE}
 echo 'export THIS_IP=$(curl icanhazip.com)' >> ${THIS_FILE}
 echo "export K3S_TOKEN='${SECRET}'" >> ${THIS_FILE}
-echo 'export INSTALL_K3S_EXEC="server --tls-san $THIS_IP --flannel-backend=wireguard-native --disable=traefik"' >> ${THIS_FILE}
-echo "curl -sfL https://get.k3s.io | sh -s - --server https://${SERVER_IP}:6443" >> ${THIS_FILE}
+echo "export K3S_URL='https://${SERVER_IP}:6443'" >> ${THIS_FILE}
+echo 'export INSTALL_K3S_EXEC="server --tls-san $THIS_IP --flannel-backend=wireguard-native --disable=traefik --secrets-encryption"' >> ${THIS_FILE}
+echo "curl -sfL https://get.k3s.io | sh -s -" >> ${THIS_FILE}
