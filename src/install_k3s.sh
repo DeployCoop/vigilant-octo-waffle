260a86e7d59e74e24b0ee4ebadbb0bb5bd0ad9ff
Joshua Edward McLaughlin Cox <josh@webhosting.coop>
2025-11-06 09:15:28 -0600

    refactor

--- src/install_k3s.sh
+++ src/install_k3s.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-export THIS_IP=$(curl icanhazip.com)
-export INSTALL_K3S_EXEC_COMMON="server --tls-san $THIS_IP --flannel-backend=wireguard-native --disable=traefik --secrets-encryption"
+THIS_FILE=./.secrets/k3s_init_node.sh
+THIS_IP=$(curl icanhazip.com)
+INSTALL_K3S_EXEC_COMMON="server --tls-san $THIS_IP --flannel-backend=wireguard-native --disable=traefik --secrets-encryption"
 
 # test alpine
 . /etc/os-release
@@ -12,15 +13,13 @@ fi
 export INSTALL_K3S_EXEC="${INSTALL_K3S_EXEC_COMMON} --cluster-init"
 curl -sfL https://get.k3s.io | sh -s -
 
-# grab some variables
+# grab the token 
 SECRET=$(cat /var/lib/rancher/k3s/server/node-token)
-THIS_FILE=./.secrets/k3s_init_node.sh
-SERVER_IP=$(curl icanhazip.com)
 
 # make the k3s init node script
 echo '#!/bin/bash' > ${THIS_FILE}
 echo 'export THIS_IP=$(curl icanhazip.com)' >> ${THIS_FILE}
 echo "export K3S_TOKEN='${SECRET}'" >> ${THIS_FILE}
-echo "export K3S_URL='https://${SERVER_IP}:6443'" >> ${THIS_FILE}
+echo "export K3S_URL='https://${THIS_IP}:6443'" >> ${THIS_FILE}
 echo "export INSTALL_K3S_EXEC='${INSTALL_K3S_EXEC_COMMON}'" >> ${THIS_FILE}
 echo "curl -sfL https://get.k3s.io | sh -s -" >> ${THIS_FILE}
