996e192237ea6fa4fcd5891edc976dde2bb0c4c0
Joshua Edward McLaughlin Cox <josh@webhosting.coop>
2025-11-07 10:50:45 -0600

    bash

--- src/prepDebian.sh
+++ src/prepDebian.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env
+#!/usr/bin/env bash
 apt-get install -y zsh rsync git curl btop htop vim net-tools dnsutils sudo jq kubecolor kubectl kubectx kubetail parallel pwgen sshuttle
 src/installHelm.sh
 src/installArgoCLI.sh
