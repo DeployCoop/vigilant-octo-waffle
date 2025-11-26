284e50cffd0016636a86d37c03a8e2ba7655efd5
Joshua Edward McLaughlin Cox <josh@webhosting.coop>
2025-11-26 09:37:02 -0600

    feat: improve argoRunner bash script with envsubst and argocd app creation

--- src/argoRunner.bash
+++ src/argoRunner.bash
@@ -5,6 +5,7 @@ source src/merge2yaml.bash
 argoRunner () {
   ARGORUNNER_TMP=$(mktemp -d --suffix .tmp.d)
   ARGORUNNER_ENVSUBST=${ARGORUNNER_TMP}/envsubst.sh
+  ARGORUNNR_INSTALL_TMP=${ARGORUNNER_TMP}/argorunnr.sh
   trap "rm -rf ${ARGORUNNER_TMP}" EXIT
   this_cwd=$(pwd)
   if [[ $# -eq 1 ]]; then
@@ -25,15 +26,20 @@ argoRunner () {
     cp -a "argo/${THIS_THING}" "${ARGORUNNER_TMP}/"
   fi
   echo '#!/bin/sh' > ${ARGORUNNER_ENVSUBST}
-  echo 'set -eux' > ${ARGORUNNER_ENVSUBST}
+  echo 'set -eu' > ${ARGORUNNER_ENVSUBST}
   echo 'envsubst \' >> ${ARGORUNNER_ENVSUBST}
   #cat src/default.env|grep -v '^$'|grep -v '^#'|awk '{print $2}'|sed 's/"\${\(.*\):=.*/${\1}/'|tr '\n' ' '|sed "s/^/' /"|sed "s/$/'/"| sed 's/$/ \\\n/' >> ${ARGORUNNER_ENVSUBST}
   convert_default_env_to_envsubst >> ${ARGORUNNER_ENVSUBST}
   echo '< src/openebs-values.tpl \' >> ${ARGORUNNER_ENVSUBST}
-  echo "> ${OPENEBS_INSTALL_TMP}" >> ${ARGORUNNER_ENVSUBST}
-  envsubst "${ENV_SUBST_EXCLUDES}"
-    < "${ARGORUNNER_TMP}/${THIS_THING}/argocd.yaml" \
-    | argocd app create --name "${THIS_THING}" --grpc-web -f -
-
+  echo "> ${ARGORUNNR_INSTALL_TMP}" >> ${ARGORUNNER_ENVSUBST}
+  #bash ${OPENEBS_ENVSUBST} 
+  # envsubst "${ENV_SUBST_EXCLUDES}"
+  #   < "${ARGORUNNER_TMP}/${THIS_THING}/argocd.yaml" \
+  #   | argocd app create --name "${THIS_THING}" --grpc-web -f -
+  #
+  #envsubst "${ENV_SUBST_EXCLUDES}"
+  bash ${ARGORUNNER_ENVSUBST} 
+    < "${ARGORUNNER_TMP}/${THIS_THING}/argocd.yaml"
+  argocd app create --name "${THIS_THING}" --grpc-web -f ${ARGORUNNR_INSTALL_TMP}
   cd "${this_cwd}"
 }
