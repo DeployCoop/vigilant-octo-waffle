c39552b35544f3b87ccb64198d3505aecd5c667e
Joshua Edward McLaughlin Cox <josh@webhosting.coop>
2025-11-30 16:09:38 -0600

    adding some w8s

--- src/mariadb-operator.sh
+++ src/mariadb-operator.sh
@@ -4,26 +4,13 @@ source src/common.sh
 
 main () {
   set -eu
-  # Depending on what your thing is you might use a mix of these lines
-  #
   # initializer "${this_cwd}/init/pre-${THIS_THING}"
   ARGOCD_CREATE_APP_EXTRA_ARGS='--helm-skip-crds'
   argoRunner "${THIS_THING}"
-  # w8_pod "${THIS_NAMESPACE}" "${THIS_THING}-${THIS_NAME}"
-  # initializer "${this_cwd}/init/${THIS_THING}"
-  # if [[ ${THIS_CLUSTER_INGRESS} == "traefik" ]]; then
-  #   initializer "${this_cwd}/init/${THIS_THING}_traefik"
-  # elif [[ ${THIS_CLUSTER_INGRESS} == "nginx" ]]; then
-  #   echo 'unimplemented'
-  #   exit 1
-  #   initializer "${this_cwd}/init/${THIS_THING}_nginx"
-  # elif [[ ${THIS_CLUSTER_INGRESS} == "haproxy" ]]; then
-  #   echo 'unimplemented'
-  #   exit 1
-  #   initializer "${this_cwd}/init/${THIS_THING}_haproxy"
-  # else
-  #   echo 'unrecognized ingress'
-  #   exit 1
-  # fi
+  # w8 on mariadb-operator-example-mariadb-operator-webhook
+  w8_pod "${THIS_NAMESPACE}" "${THIS_THING}-${THIS_NAME}-${THIS_THING}"
+  w8_pod "${THIS_NAMESPACE}" "${THIS_THING}-${THIS_NAME}-${THIS_THING}-webhook"
+  w8_pod "${THIS_NAMESPACE}" "${THIS_THING}-${THIS_NAME}-${THIS_THING}-cert"
+  initializer "${this_cwd}/init/${THIS_THING}"
 }
 time main
