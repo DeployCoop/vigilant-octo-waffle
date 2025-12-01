881a437455291c3405c1373ebd792e405e9a9a7b
Joshua Edward McLaughlin Cox <josh@webhosting.coop>
2025-11-30 19:01:36 -0600

    feat: update mariadb-operator to use secrets for root credentials

--- src/mariadb-operator.sh
+++ src/mariadb-operator.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 THIS_THING=mariadb-operator
+THIS_SECRET_KEY=${THIS_MARIADB_ROOT_KEY}
 source src/common.sh
 
 main () {
   set -eu
+  src/mksecret.sh "${THIS_MARIADB_ROOT_SECRET}" "${THIS_MARIADB_ROOT_PASSWORD}"
   # initializer "${this_cwd}/init/pre-${THIS_THING}"
   ARGOCD_CREATE_APP_EXTRA_ARGS='--helm-skip-crds'
   argoRunner "${THIS_THING}"
--- src/mksecret.sh
+++ src/mksecret.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 source src/sourceror.bash
+: "${THIS_SECRET_KEY:=username}"
 
+if [[ $# -eq 2 ]]; then
 MK_NEW_SECRET_NAME=$1
 MK_NEW_SECRET_CONTENT=$2
+else
+    echo "wrong number of args $#"
+    exit 1
+fi
 
 kubectl create secret generic "${MK_NEW_SECRET_NAME}" \
     -n "${THIS_NAMESPACE}" \
-    --from-literal=username="${MK_NEW_SECRET_CONTENT}"
+    --from-literal="${THIS_SECRET_KEY}"="${MK_NEW_SECRET_CONTENT}"
