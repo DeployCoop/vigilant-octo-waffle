19859e460a0e2ccd528900aaad826f7985e12a66
Joshua Edward McLaughlin Cox <josh@webhosting.coop>
2026-01-02 10:54:33 -0600

    Adding rook clusters

--- src/rook-ceph-tools.sh
+++ src/rook-ceph-tools.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
-THIS_THING=rook-ceph-operator
+THIS_THING=rook-ceph-tools
 source src/common.sh
 
 main () {
   set -eu
   # Depending on what your thing is you might use a mix of these lines
   #
-  initializer "${this_cwd}/init/pre-${THIS_THING}"
+  # initializer "${this_cwd}/init/pre-${THIS_THING}"
   # ARGOCD_CREATE_APP_EXTRA_ARGS='--helm-skip-crds'
-  argoRunner "${THIS_THING}"
+  # argoRunner "${THIS_THING}"
   # w8_pod "${THIS_NAMESPACE}" "${THIS_THING}-${THIS_NAME}"
-  #initializer "${this_cwd}/init/${THIS_THING}"
+  initializer "${this_cwd}/init/${THIS_THING}"
   # if [[ ${THIS_CLUSTER_INGRESS} == "traefik" ]]; then
   #   initializer "${this_cwd}/init/${THIS_THING}_traefik"
   # elif [[ ${THIS_CLUSTER_INGRESS} == "nginx" ]]; then
@@ -25,7 +25,6 @@ main () {
   #   echo 'unrecognized ingress'
   #   exit 1
   # fi
-  sleep 2
   w8_all_namespace "${THIS_NAMESPACE}" 
 }
 time main
