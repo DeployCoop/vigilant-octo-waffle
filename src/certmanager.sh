df91a2ba6f62a910142cb890eeb647ccfbe8a601
Thoth <josh@webhosting.coop>
2026-02-12 17:45:19 -0600

    CRDs

--- src/certmanager.sh
+++ src/certmanager.sh
@@ -3,14 +3,24 @@ THIS_THING=certmanager
 source src/common.sh
 
 set -eu
+
+kubectl apply \
+  --server-side \
+  -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.4.1/standard-install.yaml
+
+  #--version v1.16.3 \
+  #cert-manager oci://quay.io/jetstack/charts/cert-manager \
 helm upgrade --install \
   cert-manager cert-manager \
   --repo https://charts.jetstack.io \
   --namespace cert-manager --create-namespace \
+  --set config.apiVersion="controller.config.cert-manager.io/v1alpha1" \
+  --set config.kind="ControllerConfiguration" \
+  --set config.enableGatewayAPI=true \
   --wait \
-  --version v1.16.3 \
   --set prometheus.enabled=true \
   --set crds.enabled=true
+
 kubectl -n cert-manager get pod
 set +e
 src/mkcert.sh
--- src/istio.sh
+++ src/istio.sh
@@ -25,6 +25,9 @@ main () {
   #   echo 'unrecognized ingress'
   #   exit 1
   # fi
+  kubectl get crd gateways.gateway.networking.k8s.io &> /dev/null || \
+  { kubectl kustomize "github.com/kubernetes-sigs/gateway-api/config/crd?ref=v1.4.0" | kubectl apply -f -; }
+
   istioctl install \
     --set profile=${THIS_ISTIO_PROFILE} \
     ${THIS_ISTIO_EXTRA_ARGS} \
