78b6e4c8a3ba96d7ca979f55c501670233187e99
Joshua Edward McLaughlin Cox <josh@webhosting.coop>
2026-08-05 17:15:35 -0500

    openebs_install_argocd

--- src/openebs.sh
+++ src/openebs.sh
@@ -8,19 +8,19 @@ OPENEBS_ENVSUBST=$(mktemp)
 OPENEBS_INSTALL_TMP=$(mktemp)
 trap "rm -f ${OPENEBS_ENVSUBST} ${OPENEBS_INSTALL_TMP}" EXIT
 
-main () {
+main() {
   #set -eux
   set -eu
 
   src/kmod.sh nvme_tcp
   #kubectl krew install mayastor openebs
-  echo '#!/bin/sh' > ${OPENEBS_ENVSUBST}
-  echo 'set -eux' > ${OPENEBS_ENVSUBST}
-  echo 'envsubst \' >> ${OPENEBS_ENVSUBST}
-  convert_default_env_to_envsubst >> ${OPENEBS_ENVSUBST}
-  echo -n "< ${THIS_OPENEBS_HELM_TEMPLATE} " >> ${OPENEBS_ENVSUBST}
-  echo '\' >> ${OPENEBS_ENVSUBST}
-  echo "> ${OPENEBS_INSTALL_TMP}" >> ${OPENEBS_ENVSUBST}
+  echo '#!/bin/sh' >${OPENEBS_ENVSUBST}
+  echo 'set -eux' >${OPENEBS_ENVSUBST}
+  echo 'envsubst \' >>${OPENEBS_ENVSUBST}
+  convert_default_env_to_envsubst >>${OPENEBS_ENVSUBST}
+  echo -n "< ${THIS_OPENEBS_HELM_TEMPLATE} " >>${OPENEBS_ENVSUBST}
+  echo '\' >>${OPENEBS_ENVSUBST}
+  echo "> ${OPENEBS_INSTALL_TMP}" >>${OPENEBS_ENVSUBST}
 
   if [[ ${THIS_OPENEBS_INSTALL_METHOD} == "argocd" ]]; then
     argoRunner "$THIS_THING"
@@ -30,16 +30,16 @@ main () {
     #${OPENEBS_ENVSUBST} \
     #  < src/openebs-values.tpl \
     #  > ${OPENEBS_INSTALL_TMP}
-    #cat ${OPENEBS_ENVSUBST} 
+    #cat ${OPENEBS_ENVSUBST}
     #exit 1
-    bash ${OPENEBS_ENVSUBST} 
+    bash ${OPENEBS_ENVSUBST}
     helm install openebs-${THIS_NAME} \
       --namespace openebs \
       openebs/openebs \
       --timeout 10m0s \
       --create-namespace \
       -f ${OPENEBS_INSTALL_TMP}
-      #-f src/openebs-values.yaml
+    #-f src/openebs-values.yaml
   else
     echo 'unknown installation method'
     exit 1
@@ -48,11 +48,9 @@ main () {
   w8_pod ${THIS_OPENEBS_NAMESPACE} openebs-${THIS_NAME}-localpv-provisioner
   w8_pod ${THIS_OPENEBS_NAMESPACE} openebs-${THIS_NAME}-lvm-localpv-controller
   w8_pod ${THIS_OPENEBS_NAMESPACE} openebs-${THIS_NAME}-lvm-localpv-node
-  w8_all_namespace "${THIS_OPENEBS_NAMESPACE}" 
+  w8_all_namespace "${THIS_OPENEBS_NAMESPACE}"
   set -e
   initializer "$this_cwd/init/openebs"
 }
 
 time main
-
-
